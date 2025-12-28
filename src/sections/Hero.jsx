614b1490eb83d6e1fdb5bdf51feeeca458793d54
DariushOps <overlord127001@gmail.com>
2025-12-28 18:10:40 +0330

    updated hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,7 @@
 import { useMemo } from "react";
+import Button from "../components/Button";
+import { ArrowRight, Download, Github, Instagram, Send } from "lucide-react";
+import AnimatedBorderButton from "../components/AnimatedBorderButton";
 
 export default function Hero() {
   const dots = useMemo(() => {
@@ -35,6 +38,76 @@ export default function Hero() {
             }}
           />
         ))}
+      </div>
+
+      {/* {Content} */}
+      <div className="container mx-auto px-6 pt-32 pb-20 relative z-10">
+        <div className="grid lg:grid-cols-2 gap-12 items-center">
+          <div className="space-y-8">
+            <div className="animate-fade-in">
+              <span className="inline-flex text-sm text-primary items-center gap-2 px-4 py-2 glass rounded-full">
+                <span className="w-2 h-2 rounded-full bg-primary animate-pulse" />
+                Front-End Developer . React Specialist
+              </span>
+            </div>
+
+            {/* Headline */}
+            <div className="space-y-4">
+              <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold leading-tight animate-fade-in animation-delay-100">
+                Crafting <span className="text-primary glow-text">digital</span>
+                <br />
+                experiences with
+                <br />
+                <span className="font-serif italic font-normal text-white">
+                  precision.
+                </span>
+              </h1>
+              <p className="text-lg text-muted-foreground max-w-lg animate-fade-in animation-delay-200">
+                Hi, I'm Amir Muhammad Jafari — a frontend developer focused on
+                building modern web experiences with React, Next.js, and
+                TypeScript. I create scalable, high-performance applications
+                that are intuitive, reliable, and enjoyable to use.
+              </p>
+            </div>
+
+            {/* {CTAs} */}
+            <div className="flex flex-wrap gap-4 animate-fade-in animation-delay-300">
+              <Button size="lg">
+                Contact Me <ArrowRight className="w-5 h-5" />{" "}
+              </Button>
+              <AnimatedBorderButton>
+                <Download className="w-5 h-5" />
+                Download CV
+              </AnimatedBorderButton>
+            </div>
+
+            {/* {Social link} */}
+            <div>
+              <span>Follow :</span>
+              {[
+                {
+                  icon: Github,
+                  href: "#",
+                },
+                {
+                  icon: Send,
+                  href: "#",
+                },
+                {
+                  icon: Instagram,
+                  href: "#",
+                },
+              ].map((social, index) => {
+                const Icon = social.icon;
+                return (
+                  <a href={social.href} key={index}>
+                    {<Icon />}
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
